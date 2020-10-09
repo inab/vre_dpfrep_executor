df25088251952cfefeb9a78b0c14b5ac84938b4d
maky1112 <62390516+maky1112@users.noreply.github.com>
2020-10-09 17:40:41 +0200

    Update run_dpfrep.r

--- lib/run_dpfrep.r
+++ lib/run_dpfrep.r
@@ -4,30 +4,29 @@ library(reshape2)
 library(prodlim)
 library(openxlsx)
 
-source('/home/user/vre_dpfrep_executor/lib/dpfrep_functions.r')
+source('/home/user/vre_dpfrep_executor/lib/dpfrep_functions.R')
+
 
 message("STARTING DRUG PREDICTION PROCESS")
 
 message("Reading input arguments")
 args = commandArgs(trailingOnly = TRUE)
+if(length(args)!=4){
+  print("Please supply all arguments.")
+}
+
 input_file = args[1]
 reference_ids = args[2] 
 tumor_type = args[3]
-file_type = args[4]
-return_stdz_ES = as.logical(args[5])
+return_stdz_ES = as.logical(args[4])
 
-
-if(file_type=='CSV'){
-  exp_mtx <- as.matrix(read.csv(file = input_file, check.names = F, row.names = 1, stringsAsFactors = F))
-}else{
-  exp_mtx <- as.matrix(read.csv(file = input_file, sep='\t', check.names = F, row.names = 1, stringsAsFactors = F))
-}
-
+exp_mtx <- as.matrix(read.csv(file = input_file, check.names = F, row.names = 1, stringsAsFactors = F))
 reference_ids <- strsplit(reference_ids,',')[[1]]
 reference_ids <- intersect(reference_ids,colnames(exp_mtx))
 
-if(!isEmpty(reference_ids)){
+if(reference_ids!='NULL'){
   message('Computing z-scores')
+  reference_ids <- strsplit(readLines(reference_ids),',')[[1]]
   condition_ids <- setdiff(colnames(exp_mtx),reference_ids)
   exp_mtx <- sapply(condition_ids,function(i)apply(exp_mtx[,c(i,reference_ids)],1,mosaic::zscore)[1,])
 }
