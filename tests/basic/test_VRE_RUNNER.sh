4b6190e81e420d3fcbe5b8aebda297e2cae6285e
maky1112 <62390516+maky1112@users.noreply.github.com>
2021-02-04 16:15:29 +0100

    Update test_VRE_RUNNER.sh

--- tests/basic/test_VRE_RUNNER.sh
+++ tests/basic/test_VRE_RUNNER.sh
@@ -7,22 +7,24 @@
 ### * Automatically created by VRE *
 ###
 
+
 # Local installation - EDIT IF REQUIRED
-CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-TEST_DATA_DIR=$CWD
-WORKING_DIR=$TEST_DATA_DIR/run000
-TOOL_EXECUTABLE=$TEST_DATA_DIR/../../VRE_RUNNER
+WORKING_DIR=/test/execution/directory
+TOOL_EXECUTABLE=/main/mg-tool/executable.py
 
-# Running cwl_wrapper_test tool
+# Test input files
 
-if [ -d $WORKING_DIR ]; then
-  rm -r $WORKING_DIR/
-  mkdir -p $WORKING_DIR
-else mkdir -p $WORKING_DIR; fi
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TEST_DATA_DIR=$CWD/json/0_RunDpFrEPpipeline
+
+
+# Running DpFrEP tool ...
+
+if [ -d  $WORKING_DIR ]; then rm -r $WORKING_DIR/; mkdir -p $WORKING_DIR; else mkdir -p $WORKING_DIR; fi
 cd $WORKING_DIR
 
 echo "--- Test execution: $WORKING_DIR"
-echo "--- Start time: $(date)"
+echo "--- Start time: `date`"
 
 time $TOOL_EXECUTABLE --config $TEST_DATA_DIR/config.json --in_metadata $TEST_DATA_DIR/in_metadata.json --out_metadata $WORKING_DIR/out_metadata.json > $WORKING_DIR/tool.log
